d04858dada0a86e475e8cab2f58811ea0db1bd02
blacf0r7 <93528309+blacf0r7@users.noreply.github.com>
2024-03-01 18:00:23 +1000

    Add files via upload

--- Новая папка (3)/index.js
+++ Новая папка (3)/index.js
@@ -32,32 +32,33 @@ for (let i = 0; i < massCard.length; i++){
 let hours = document.getElementById("hours");
 let min = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
+let start  = document.getElementById("start")
+let inputs = document.getElementsByClassName("input-time");
 
-let h = 1;
-let m = 20;
-let s = 40;
-
-// setTimeout(() => {
-//     console.log("Привет, пользователь!")
-// }, 10000);
-
-const timer = setInterval(() => {
-    seconds.innerText = s;
-    min.innerText = m;
-    hours.innerText = h;
-    s = s - 1;
-    if (s == 0) {
-        m--;
-        s = 60;
-    }
-    if (m == 0) {
-        h--;
-        m = 60;
-    }
-    if (h < 0) {
-        clearInterval(timer);
-        seconds.innerText = 0;
-        min.innerText = 0;
-        hours.innerText = 0;
-    }
-}, 1000);
+// console.log("inputi", inputs);
+// ДОВЕСТИ ДО УМА
+start.onclick = () => {
+    let h = inputs[0].value;
+    let m = inputs[1].value;
+    let s = inputs[2].value;
+    const timer = setInterval(() => {
+        seconds.innerText = s;
+        min.innerText = m;
+        hours.innerText = h;
+        s = s - 1;
+        if (s == 0) {
+            m--;
+            s = 60;
+        }
+        if (m == 0) {
+            h--;
+            m = 60;
+        }
+        if (h < 0) {
+            clearInterval(timer);
+            seconds.innerText = 0;
+            min.innerText = 0;
+            hours.innerText = 0;
+        }
+    }, 1000);
+}
